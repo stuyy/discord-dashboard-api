51814c613ebf8693d6d2b42e194f9d3dafc01f2f
Anson <ansfoong@gmail.com>
2022-01-16 09:31:53 -0700

    Enable CORS & Emit Ban Add/Remove Events

--- src/websocket/socket.ts
+++ src/websocket/socket.ts
@@ -7,7 +7,11 @@ import {
 import { Server } from 'socket.io';
 import { GuildConfiguration } from '../utils/typeorm/entities/GuildConfiguration';
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: ['http://localhost:3000'],
+  },
+})
 export class WebsocketHandler {
   @WebSocketServer()
   ws: Server;
@@ -25,11 +29,13 @@ export class WebsocketHandler {
   guildBanAddHandler(@MessageBody() data: any) {
     console.log('Guild Ban Add...');
     console.log(data);
+    this.ws.emit('guildBanAdd', data);
   }
 
   @SubscribeMessage('guildBanRemove')
   guildBanRemoveHandler(@MessageBody() data: any) {
     console.log('Guild Ban Remove...');
     console.log(data);
+    this.ws.emit('guildBanRemove', data);
   }
 }
